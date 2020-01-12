fc76f079d41189e09bd898733e535b153e5b9300
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-01-12 12:42:07 -0300

    💄 change icons

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: 'JsonLogic Playground',
+    themeColor: '#292d3e',
+    msTileColor: '#292d3e',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  }
+}
